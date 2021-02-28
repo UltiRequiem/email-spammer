84d7d6970e3c001c0616325acd30d9c15d47bf54
Eliaz Bobadilla <71897736+UltiRequiem@users.noreply.github.com>
2021-02-28 12:59:07 -0500

    Whit victims.txt

--- main.py
+++ main.py
@@ -1,23 +1,38 @@
 # -*- coding: utf-8 -*-
-import smtplib, ssl
+from email.mime.text import MIMEText
+from time import sleep
+from random import randint
 
-port = 465
-smtp_server = "smtp.gmail.com"
+username =  str(input('Type your email: ')) 
+password = str(input("Type your password: "))
 
-sender_email = str(input('Type your email: '))  
-receiver_email = input('Type the email of the receiver: ')
-password = input("Type your password and press enter: ")
+def start_server():
+    server = smtplib.SMTP('smtp.gmail.com')
+    server.ehlo()
+    server.starttls()
+    server.login(username, password)
+    return server
 
-subject = str(input('Subject: '))
-subject = 'Subject: ' + subject
-text = str(input('Type your email body: '))
-message = subject + '\n' + text
+def send_emails(server):
+    emails = "".join(open("victims.txt", "r").read().split()) 
+    for email in range(100):
+        #print(str(email) + ' emails have been sent.') 
+        msg = MIMEText("This is a cool message.If you want revenge here is how: https://github.com/UltiRequiem/gmail-spammer")
+        msg['Subject'] = 'Cool Spam ' + str(randint(1, 1000)) 
+        msg['From'] = username
+        msg['To'] = emails
+        server.sendmail(username, emails, msg.as_string())
 
-veces = int(input('Type how many emails you want to send: '))
+def main():
+    while True:
+        server = start_server()
+        try:
+            send_emails(server)
+        except Exception as e:
+            sleep(60) 
 
-if __name__ == '__main__':
-    context = ssl.create_default_context()
-    with smtplib.SMTP_SSL(smtp_server, port, context=context) as server:
-        for x in range(veces):
-            server.login(sender_email, password)
-            server.sendmail(sender_email, receiver_email, message)
+if __name__ == "__main__":
+    try:
+        main()
+    except KeyboardInterrupt:
+        print("Interrupted program. Exitting...")
