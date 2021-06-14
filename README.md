80b01eb157d153a76892d0ba13a32fa8d9c40b62
Eliaz Bobadilla <eliaz.bobadilladev@gmail.com>
2021-06-14 15:13:08 -0500

    :art: Update

--- README.md
+++ README.md
@@ -1,11 +1,18 @@
 # Cool and Simple Email Spammer
 
+<p>
+<a href="https://github.com/UltiRequiem/Email-Spammer/blob/main/LICENSE"><img alt="License: MIT" src="https://black.readthedocs.io/en/stable/_static/license.svg"></a>
+<a href="https://github.com/UltiRequiem/Email-Spammer"><img alt="Code style: black" src="https://img.shields.io/badge/code%20style-black-000000.svg"></a>
+<a href="https://github.com/UltiRequiem/Email-Spammer"><img alt="Code style: black" src="https://img.shields.io/tokei/lines/github.com/UltiRequiem/Email-Spammer?color=blue&label=Total%20Lines"></a>
+</p>
+
 For this to work you need to [Activate Less Secure Apps](https://myaccount.google.com/lesssecureapps) on your google account.If you use another email, the process should be similar and it is certain that you will have to change some parts of the code.
 
-Running [main.py](./main.py)  :
+Running [main.py](./main.py) :
 [![Execution](https://user-images.githubusercontent.com/71897736/109439752-a4975500-79fd-11eb-9600-175913c729a5.png)](https://youtu.be/n0NHnISdVEw)
 (You have to click it)
 
 Check out the other branches:
+
 - [basic](https://github.com/UltiRequiem/Email-Spammer/tree/basic)
 - [classic-separate-login](https://github.com/UltiRequiem/Email-Spammer/tree/clasic-separate-login)
--- main.py
+++ main.py
@@ -3,25 +3,30 @@ from email.mime.text import MIMEText
 from time import sleep
 from random import randint
 
-username =  str(input('Type your email: ')) 
+username = str(input("Type your email: "))
 password = str(input("Type your password: "))
 
+
 def start_server():
-    server = smtplib.SMTP('smtp.gmail.com')
+    server = smtplib.SMTP("smtp.gmail.com")
     server.ehlo()
     server.starttls()
     server.login(username, password)
     return server
 
+
 def send_emails(server):
-    emails = "".join(open("receivers.txt", "r").read().split()) 
+    emails = "".join(open("receivers.txt", "r").read().split())
     for email in range(100):
-        print(str(email) + ' emails have been sent.') 
-        msg = MIMEText("This is a cool message.If you want revenge here is how: https://github.com/UltiRequiem/email-spammer")
-        msg['Subject'] = 'Cool Spam ' + str(randint(1, 1000)) 
-        msg['From'] = username
-        msg['To'] = emails
+        print(f"{email} emails have been sent.")
+        msg = MIMEText(
+            "This is a cool message. If you want revenge here is how: https://github.com/UltiRequiem/email-spammer"
+        )
+        msg["Subject"] = "Cool Spam " + str(randint(1, 1000))
+        msg["From"] = username
+        msg["To"] = emails
         server.sendmail(username, emails, msg.as_string())
+
 
 def main():
     while True:
@@ -29,7 +34,8 @@ def main():
         try:
             send_emails(server)
         except Exception as e:
-            sleep(60) 
+            sleep(60)
+
 
 if __name__ == "__main__":
     try:
